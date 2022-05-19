dc4f376962f1e0d55e7cfd97c1ee3c197712e106
shagufta <shagufta.batool8@gmail.com>
2022-05-19 09:56:00 +0200

    create component SeriesList

--- src/components/SeriesList.jsx
+++ src/components/SeriesList.jsx
@@ -3,22 +3,22 @@ import axios from "axios";
 
 const SeriesList = () => {
   const [series, setSeries] = useState([]);
-  //get the data
-  // fetch it with axios
 
   const fetchSeries = async () => {
-    const response = await axios.get("https://content.viaplay.se/pc-se/serier/samtliga");
+    const response = await axios.get(
+      "https://content.viaplay.se/pc-se/serier/samtliga"
+    );
     setSeries(response.data.results);
   };
-  const seriesList = series.map(serie) => {
+
+  const seriesList = series.map((serie) => {
     return (
-      <li key={}>
+      <li key={serie}>
         <h3>{}</h3>
       </li>
     );
-});
-  
-  //call for fetchEmployee function
+  });
+
   useEffect(() => {
     fetchSeries();
   }, []);
